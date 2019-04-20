c0a4e1acf6a03686259824267f36408dbe180d16
Danil Gavrilov <danil.gavrilov@gmail.com>
2019-04-21 00:22:28 +0700

    Initial commit

---
+++ hh.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup as bs, ResultSet
+
+headers = {'accept': '*/*',
+           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
+
+base_url = 'https://hh.ru/search/vacancy?search_period=3&area=3&text=python'
+
+def hh_parse(base_url, headers):
+    session = requests.Session()
+    request = session.get(base_url, headers=headers)
+    if request.status_code == 200:
+        soup = bs(request.content, 'html.parser')
+        # print(soup)
+        divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
+        for div in divs:
+            title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
+            href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
+        print(href)
+    else:
+        print('ERROR')
+
+hh_parse(base_url, headers)
